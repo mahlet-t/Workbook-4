ebd5dbb8e6ff5a1a4384c2cffff016b67f8919f4
mahle <mahletwondwossen71@gmail.com>
2025-05-06 12:59:01 -0500

    Exercise 3 Overloading Methods

--- HotelOperations/src/main/java/Employee.java
+++ HotelOperations/src/main/java/Employee.java
@@ -1,29 +1,35 @@
+import java.time.LocalDateTime;
+
 public class Employee {
     private String employeeId;
     private String name;
     private String department;
     private int payRate;
-    private int hoursWorked;
+    private double hoursWorked;
+    private double punchInTime;
 
-    public Employee(String employeeId, String name, String department, int payRate, int hoursWorked) {
+
+    public Employee(String employeeId, String name, String department, int payRate, double hoursWorked,double punchInTime) {
         this.employeeId = employeeId;
         this.name = name;
         this.department = department;
         this.payRate = payRate;
         this.hoursWorked = hoursWorked;
+        this.punchInTime=punchInTime;
 
     }
 
     public double getTotalPay() {
+
         return payRate*getRegularHours()+(getOverTimeHours()*payRate*1.5);
     }
 
-    public int getRegularHours() {
+    public double getRegularHours() {
         return hoursWorked-getOverTimeHours();
         }
 
 
-    public int getOverTimeHours() {
+    public double getOverTimeHours() {
         if (hoursWorked>40) {
             return hoursWorked - 40;
         }else {
@@ -36,7 +42,47 @@ public class Employee {
         return payRate;
     }
 
-    public int getHoursWorked() {
+    public double getHoursWorked() {
         return hoursWorked;
     }
-}
+     public void punchIn(double time){
+        punchInTime=time;
+         System.out.println("punched in at:"+time);
+     }
+     public void punchOut(double time){
+        double worked=time-punchInTime;
+        hoursWorked+=worked;
+         System.out.println("Punched out at:"+time);
+         System.out.println("You worked:"+ worked+" hours.");
+     }
+
+    public void punchTimeCard(double punchInTime,double punchOutTime){
+            double worked=punchOutTime-punchInTime;
+            hoursWorked+=worked;
+            System.out.println("Punched in at:"+punchInTime);
+            System.out.println("punched out at:"+punchOutTime);
+            System.out.println("you worked hours"+worked);
+        }
+        public void punchIn(){
+            LocalDateTime now=LocalDateTime.now();
+            int hour=now.getHour();
+            int minute=now.getMinute();
+            double time=hour+(minute/60.0);
+            punchInTime=time;
+            System.out.println("punched in at:"+time);
+        }
+        public void punchOut(){
+            LocalDateTime now=LocalDateTime.now();
+            int hour=now.getHour();
+            int minute=now.getMinute();
+            double time=hour+(minute/60.0);
+            double worked=time-punchInTime;
+            hoursWorked+=worked;
+            System.out.println("Punched out at:"+time);
+            System.out.println("You worked:"+ worked+" hours.");
+        }
+
+
+    }
+
+
--- HotelOperations/src/main/java/Room.java
+++ HotelOperations/src/main/java/Room.java
@@ -1,2 +1,63 @@
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public class Room {
-}
+        private int numberOfBeds;
+        private double price;
+        private boolean isOccupied;
+        private boolean isDirty;
+
+        public Room(int numberOfBeds, double price, boolean isOccupied, boolean isDirty) {
+            this.numberOfBeds = numberOfBeds;
+            this.price = price;
+            this.isOccupied = isOccupied;
+            this.isDirty=isDirty;
+        }
+
+        public int getNumberOfBeds() {
+            return numberOfBeds;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+
+        public boolean isOccupied() {
+            return isOccupied;
+        }
+
+        public boolean isAvailable() {
+          return !isDirty && !isOccupied;
+
+        }
+        public void checkIn(){
+            if (!isOccupied&&!isDirty){
+                isOccupied=true;
+                isDirty=true;
+                System.out.println("Guest checked in.");
+            }
+            else {
+                System.out.println(" Room not available for check-in. ");
+            }
+        }
+        public void checkOut(){
+          if (isOccupied){
+              isOccupied=false;
+              System.out.println("Guest checked out");
+          }
+          else {
+              System.out.println("Room is already empty.");
+          }
+        }
+        public void cleanRoom(){
+            if (isDirty){
+                isDirty=false;
+                System.out.println("Room cleaned.");
+            }
+        }
+
+
+
+    }
+
+
